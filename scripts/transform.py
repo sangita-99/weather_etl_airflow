4081b66d6a469aa201fa0e97c010e9ef8400d88a
Sangita Gurung <sangita.g5253@gmail.com>
2026-02-22 20:28:18 -0600

    Update transform.py

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,6 +1,29 @@
+import json
+import pandas as pd
+import os
+
 def transform_weather(raw_file_path):
     """
-    Transform raw JSON into clean structured format
-    and return processed file path
+    Transform raw weather JSON into structured CSV format.
+    Returns processed file path.
     """
-    pass
+
+    with open(raw_file_path, "r") as f:
+        data = json.load(f)
+
+    record = {
+        "city": data.get("name"),
+        "temperature": data.get("main", {}).get("temp"),
+        "humidity": data.get("main", {}).get("humidity"),
+        "weather": data.get("weather")[0].get("description")
+    }
+
+    df = pd.DataFrame([record])
+
+    os.makedirs("data/processed", exist_ok=True)
+
+    processed_file_path = raw_file_path.replace("raw", "processed").replace(".json", ".csv")
+
+    df.to_csv(processed_file_path, index=False)
+
+    return processed_file_path
